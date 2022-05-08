43cec6e98889e13c32c4dd8cf6f9f85f8626b862
zyl82311 <59716638+zyl82311@users.noreply.github.com>
2022-05-08 09:46:47 +0800

    Create dyjs_cookies.js

---
+++ dyjs_cookies.js
@@ -0,0 +1,10 @@
+//Cookie和x-Tt-Token去抓到的headers里搜
+let cookies = [
+    {
+        "name": "123",
+        "Cookie": "c50addbcec9fa2bbae0dc9c15a53748f",
+        "x-Tt-Token": "1651966860"
+    },
+  
+];
+module.exports = cookies;
